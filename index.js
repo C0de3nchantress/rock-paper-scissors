c31ed77f419ea5e5ada69745bcc9ee0f7c227841
C0de3nchantress <e23bscu0005@bennett.edu.in>
2024-09-22 02:59:34 +0530

    Set up the play round function, allowing us to play one round

--- index.js
+++ index.js
@@ -2,25 +2,47 @@ function getComputerChoice(){
     const num = Math.floor(Math.random() * 3);
 
     if (num == 0) {
-        return "Rock";
+        return "rock";
     } else if (num == 1) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
-function getHumanChoice(num) {
-    if (num == 0) {
-        return "Rock";
-    } else if (num == 1) {
-        return "Paper"
+function getHumanChoice() {
+    return prompt("Enter your choice (Rock, Paper or Scissors): ").trim().toLowerCase();
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`You picked ${humanChoice}, Computer picked ${computerChoice}`);
+    if (humanChoice == "rock" && computerChoice == "scissors"){
+        console.log("You win! Rock beats scissors.");
+        humanScore++;
+    } else if (computerChoice == "rock" && computerChoice == "scissors") {
+        console.log("You lose! Rock beats scissors.");
+        computerScore++;
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats paper.");
+        humanScore++;
+    } else if (computerChoice == "scissors" && humanChoice == "paper") {
+        console.log("You lose! Scissors beats paper.");
+        computerScore++;
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats rock.");
+        humanScore++;
+    } else if (computerChoice == "paper" && humanChoice == "rock") {
+        console.log("You lose! Paper beats rock.");
+        computerScore++;
     } else {
-        return "Scissors"
+        console.log("It's a tie!");
     } 
 }
 
-let humanChoice = getHumanChoice(parseInt(prompt("Enter 0 for 'Rock', Enter 1 for 'Paper', Enter '2' for Scissors: ")))
-console.log(humanChoice)
+const computerChoice = getComputerChoice(); 
+const humanChoice = getHumanChoice();
 
-console.log(getComputerChoice());
+playRound(humanChoice, computerChoice);
